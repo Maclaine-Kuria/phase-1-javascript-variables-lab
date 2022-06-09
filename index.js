efe2c53fc679522f20d6c9b1f1b3ac472ce46bdc
maclaine <maclainekuria@gmail.com>
2022-06-09 09:29:29 +0300

    commit tree 83c7403c2a35b3d33c76945c34b20cfe032d6074

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// Code your solution in this file
+function printValue(value){
+    console.info(value)
+}
+
+const companyName = "Scuber"
+printValue(companyName)
+
+let mostProfitableNeighborhood = "Chelsea"
+printValue(mostProfitableNeighborhood)
+
+let companyCeo = "Susan Smith"
+printValue(companyCeo)
